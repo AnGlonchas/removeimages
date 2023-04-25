ffbadd1953969d6a89439bd749ed0205da3163c1
AnGlonchas <128877120+AnGlonchas@users.noreply.github.com>
2023-04-25 13:16:43 -0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,21 @@
+import pygame
+
+screen = pygame.display.set_mode((30,30),pygame.SCALED | pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+img = pygame.image.load("ball.png").convert_alpha()
+click = False
+drawing = True
+while True:
+	screen.fill(120)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		if event.type == pygame.MOUSEBUTTONDOWN:#here is the event u want
+			drawing = False
+		elif event.type == pygame.MOUSEBUTTONUP:
+			drawing = True
+	if drawing:
+		screen.blit(img,(0,0))
+	click = False
+	pygame.display.flip()
+	clock.tick(60)
